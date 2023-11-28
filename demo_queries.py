98ee8b4daf17d10bfac07e75d9a15d6323ee767a
Vidya Kamath P <42544705+VidyaKamath@users.noreply.github.com>
2023-11-28 11:32:43 -0600

    Update demo_queries.py

--- demo_queries.py
+++ demo_queries.py
@@ -1,10 +1,8 @@
 # No idx table
-SELECT SUM(l_extendedprice), SUM(l_discount) FROM tpch_lineitem_10g_no_idx
-SELECT SUM(l_extendedprice) FROM tpch_lineitem_10g_no_idx GROUP BY l_shipdate
+SELECT SUM(l_extendedprice), SUM(l_quantity) FROM tpch_lineitem_10g_no_idx GROUP BY l_shipdate
 
 # Star tree idx table T=1000
-SELECT SUM(l_extendedprice), SUM(l_discount) FROM tpch_lineitem_10g_star_idx_1000
-SELECT SUM(l_extendedprice) FROM tpch_lineitem_10g_star_idx_1000 GROUP BY l_shipdate
+SELECT SUM(l_extendedprice), SUM(l_quantity) FROM tpch_lineitem_10g_star_idx_1000 GROUP BY l_shipdate
 
 
 # No impact queries
